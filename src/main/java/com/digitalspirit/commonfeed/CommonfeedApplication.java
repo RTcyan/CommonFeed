fa45b1364332d3f631c85ea69f2c48cceef6251c
Maxim <mgudkov@digital-spirit.ru>
2023-06-27 10:47:28 +0300

    Initial commit

---
+++ src/main/java/com/digitalspirit/commonfeed/CommonfeedApplication.java
@@ -0,0 +1,13 @@
+package com.digitalspirit.commonfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonfeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonfeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalspirit/commonfeed/CommonfeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalspirit.commonfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonfeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
